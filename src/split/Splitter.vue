ec385b9e03511a1feedeae9179163b5b3e4b0dfc
liuyiming <liuyiming@nowcoder.com>
2021-12-23 21:12:57 +0800

    feat: split

--- src/split/Splitter.vue
+++ src/split/Splitter.vue
@@ -19,12 +19,16 @@ const props = withDefaults(defineProps<Props>(), {
 const slots = useSlots();
 const { horizontal } = toRefs(props);
 
+const handleMouseenter = () => {
+    console.log(12345);
+};
+
 const splitterMarkerVNode = computed(() => {
     return createVNode('div', { class: 'mc-splitter__marker' });
 });
 
 const Render = () => {
-    return createVNode('div', { class: 'mc-splitter' }, [splitterMarkerVNode.value]);
+    return createVNode('div', { class: 'mc-splitter', onMouseenter: handleMouseenter }, [splitterMarkerVNode.value]);
 };
 </script>
 
